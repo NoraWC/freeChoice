a763a2240f4260c446e0c9c172bc3927408943fa
Nora Coates <noracoates@students.berkeley.net>
2018-03-13 10:34:26 -0700

    choice options

--- freeChoice.js
+++ freeChoice.js
@@ -1,4 +1,11 @@
-var TRIES = 0;
+var RESULT_ARRAY = [];
+
+function clearAll() {
+    $('#hi').html("");
+    RESULT_ARRAY=[];
+    $('#warning').html("");
+}
+
 function go(questions, category, difficulty, multi) {
 
     var num = "amount=";
@@ -35,12 +42,12 @@ function go(questions, category, difficulty, multi) {
         success: function (result) {
             if(result.response_code !== 1) {
                 console.log(result.results);
+                RESULT_ARRAY = result.results;
+                console.log(RESULT_ARRAY);
                 setUp(result.results);
             } else {
-                if(TRIES<=0) {
-                    alert("Could not find enough questions in that category! Trying fewer...");
-                    TRIES++;
-                }
+                $('#warning').html("Could not find enough questions in that category! Try another level or another category if nothing appears.");
+
                 go(questions-1, category, difficulty, multi);
             }
 
@@ -55,8 +62,8 @@ function go(questions, category, difficulty, multi) {
 function setUp(resultArray) {
     var htm = "";
     for(var i = 0; i < resultArray.length; i++) {
-        htm += "<tr><td>"+resultArray[i].difficulty+"</td><td>"+resultArray[i].category+"</td><td>";
-        htm += resultArray[i].question+"</td><td><form>";
+        htm += "<tr id = 'row"+i+"'><td>"+resultArray[i].difficulty+"</td><td>"+resultArray[i].category+"</td><td>";
+        htm += resultArray[i].question+"</td><td id = 'actually"+i+"'></td><td><form>";
         var answers = [];
 
         answers = answers.concat(resultArray[i].incorrect_answers);
@@ -67,26 +74,38 @@ function setUp(resultArray) {
 
         for(var x = 0; x < answers.length; x ++) {
             if(x === pos) {
-                htm += "<input type = 'radio' class = 'choice"+i+"' name = 'rightChoice' id ='answer"+x+"' value = 'answer"+x+">"
+                htm += "<input type = 'radio' class = 'rightanswer' name = 'choice"+i+"' value = 'answer"+x+">"
             } else {
-                htm += "<input type = 'radio'  class = 'choice"+i+"' name = 'wrongChoice' id ='answer"+x+"' value = 'answer"+x+">";
+                htm += "<input type = 'radio'  class = 'wronganswer' name = 'choice"+i+"' value = 'answer"+x+">";
             }
 
             htm += "<label for = 'answer"+x+"'>"+answers[x]+"</label>";
         }
         htm += "</form></tr>";
     }
-    //runs triviaSubmit with objects, not array--unexpected identifier
-    resultArray = resultArray.toString();
-    console.log("<button id = 'finished' onclick = 'triviaSubmit("+resultArray+");'>Finished!</button>");
-    $('#done').html("<button id = 'finished' onclick = 'triviaSubmit("+resultArray+");'>Finished!</button>");
+    $('#done').html("<button id = 'finished' onclick = 'triviaSubmit();'>Finished!</button>");
     $('#hi').html(htm);
 }
 
-function triviaSubmit(arr) {
-    arr = arr.split(',');
-    for(var i = 0; i < arr.length; i++) {
-        var choiceArr = [];
-        choiceArr.push(document.getElementsByClassName('choice'+i)); //the choice radio buttons
+function triviaSubmit() {
+    for(var i = 0; i < RESULT_ARRAY.length; i++) {
+
+        var choiceArr = document.getElementsByName('choice'+i); //the choice radio buttons
+
+        for(var x = 0; x < choiceArr.length; x ++) {
+
+            if(choiceArr[x].checked) {
+
+                if(choiceArr[x].className === 'rightanswer') {
+                    document.getElementById('row'+i).className = 'correct';
+                    console.log(i+'correct');
+
+                } else {
+                    document.getElementById('row'+i).className = 'incorrect';
+                    console.log(i+'incorrect');
+                    $('#actually'+i).html('Sorry, but you should have picked '+RESULT_ARRAY[i].correct_answer);
+                }
+            }
+        }
     }
 }
