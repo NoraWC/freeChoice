269e45953600a678b44d0efdd875eb6c5f1d4a8e
Nora Coates <noracoates@students.berkeley.net>
2018-03-20 10:34:32 -0700

    choice options

--- freeChoice.js
+++ freeChoice.js
@@ -2,6 +2,8 @@ var CORRECT_ANSWERS = 0;
 function clearAll() {
     $('#hi').html("");
     $('#warning').html("");
+    $('#result').html("");
+    document.getElementById('result').className = 'secret';
     CORRECT_ANSWERS = 0;
 }
 var FIRST_QS = 0;
@@ -44,7 +46,7 @@ function go(questions, category, difficulty, multi) {
         success: function (result) {
             if(result.response_code !== 1) {
                 console.log(result.results);
-                setUpTwo(result.results);
+                setUp(result.results);
             } else if (questions > 1) {
                 $('#warning').html("Not enough questions were found! You may have fewer questions or a different difficulty level.");
                 go(questions-1, category, difficulty, multi);
@@ -60,23 +62,24 @@ function go(questions, category, difficulty, multi) {
     });
 }
 
-function setUpTwo(resultArray) {
+function setUp(resultArray) {
     var category = parseInt(document.getElementById("category").selectedIndex);
     if(category === 0) {
         category = 9;
     } else {
         category += 8;
     }
-    var fin = "<div id = 'cat-dif'>"+$('#'+category).html()+": "+resultArray[0].difficulty+"</div>";
+    $('#cat-dif').html($('#'+category).html()+": "+resultArray[0].difficulty)
+    var fin = "";
     for (var i = 0; i < resultArray.length; i++) {
         if(i > 0) {
             fin += "<div class = 'secret' id = 'contain" + i + "'>";
         } else {
             fin += "<div class = 'container' id = 'contain" + i + "'>";
         }
-        fin += "<div class = 'question' id = 'quest"+i+"'>"+resultArray[i].question+"</div>";
-        fin += "<div class = 'actually' id = 'act"+i+"'></div>";
-        fin += "<div class = 'buttons' id = 'choices"+i+"'>";
+        fin += "<span class = 'question' id = 'quest"+i+"'>"+resultArray[i].question+"</span>";
+        fin += "<span class = 'actually' id = 'act"+i+"'></span>";
+        fin += "<span class = 'buttons' id = 'choices"+i+"'>";
 
         var answers = [];
 
@@ -96,39 +99,46 @@ function setUpTwo(resultArray) {
 
             fin += "<span id = 'answer"+x+"'>"+answers[x]+"</span><br>";
         }
-        fin += "</div><button id = 'finished' onclick = 'moveOn("+i+", "+resultArray.length+");'>Finished!</button></div>";
+        fin += "</span><button id = 'finished' onclick = 'moveOn("+i+", "+resultArray.length+");'>Finished!</button></div>";
     }
 
     $('#display').html(fin);
 }
 
 function moveOn(i, length) {
-    //runs one too many times
-    if( i < length) {
-        var next = i+1;
 
-        var choiceArr = document.getElementsByName('choice'+i); //the choice radio buttons
+    var next = i+1;
+    //1 more than the element we're on
 
-        for(var x = 0; x < choiceArr.length; x ++) {
+    $('#contain'+i).hide();
 
-            if(choiceArr[x].checked) {
-
-                if(choiceArr[x].className === 'rightanswer') {
-                    $('#result').className = 'correct';
-                    CORRECT_ANSWERS++;
-                    $('#result').html('Question ' + next + ' of '+ length+': '+$('#quest'+i).html() + ' Correct! You chose ' +choiceArr[x].value);
-
-                } else {
-                    $('#result').className = 'incorrect';
-                    var rightAns = document.getElementById('right'+i).value;
-                    $('#result').html('Question ' + next + ' of '+ length+': '+$('#quest'+i).html() + ' Sorry! You picked '+choiceArr[x].value+' but you should have picked '+rightAns+'.');
-                }
-            }
-        }
-        $('#contain'+i).hide();
-        document.getElementById('score').innerHTML = "Score: "+CORRECT_ANSWERS + "/"+length;
+    if (next < length) {
+        //show the next element
         document.getElementById('contain'+next).classList.remove('secret');
         document.getElementById('contain'+next).classList.add('container');
-    }
 
+    }
+    var choiceArr = document.getElementsByName('choice'+i); // list of the choice radio buttons
+
+    for(var x = 0; x < choiceArr.length; x ++) {
+
+        if(choiceArr[x].checked) {
+            //if the current choice was selected, check if it's correct
+            if(choiceArr[x].className === 'rightanswer') {
+                //if they got it right, display a win message
+
+                document.getElementById('result').className = 'correct';
+                CORRECT_ANSWERS++;
+                $('#result').html('Question ' + next + ' of '+ length+': '+$('#quest'+i).html() + '<br> Correct! You chose ' +choiceArr[x].value);
+
+            } else {
+                //if not, display a lose message
+                document.getElementById('result').className = 'incorrect';
+                var rightAns = document.getElementById('right'+i).value;
+                $('#result').html('Question ' + next + ' of '+ length+': '+$('#quest'+i).html() + '<br> Sorry! You picked '+choiceArr[x].value+' but you should have picked '+rightAns+'.');
+            }
+        }
+    }
+    //update score
+    $('#score').html("Score: "+CORRECT_ANSWERS + "/"+length);
 }
