57ffbcd6b887bfdcb6471afefe45e52bd94c3301
NoraCoates <nora.coates@gmail.com>
2018-03-20 20:57:19 -0700

    zDS

--- freeChoice.js
+++ freeChoice.js
@@ -1,41 +1,53 @@
-var CORRECT_ANSWERS = 0;
+//total correct answers in a session
+var SCORE = 0;
+//all questions attempted so far
+var QS_SO_FAR = 0;
+
 function clearAll() {
+    //resets all displayed values
+    //doesn't need to reset questions because the next one will come up anyway
     $('#hi').html("");
     $('#warning').html("");
     $('#result').html("");
     document.getElementById('result').className = 'secret';
-    CORRECT_ANSWERS = 0;
 }
-var FIRST_QS = 0;
-function go(questions, category, difficulty, multi) {
 
+
+function go(questions, category, difficulty, multi) {
+    console.log(difficulty);
+    //takes the number of questions from the html form (if they didn't enter any, defaults to 5
     var num = "amount=";
-    if(questions > 0 || questions <= 50) {
-        num += questions.toString();
-        if(FIRST_QS === 0) {
-            FIRST_QS = questions;
-        }
-    } else {
+    if(questions === undefined || questions === NaN) {
         num += "5";
+    } else {
+        if(questions > 0 || questions <= 50) {
+            num += questions.toString();
+        } else {
+            num += "5";
+        }
     }
 
 
+    //takes the category selected (the list of categories in the API starts at 8, so it adds 8 to the index)
+    //defaults to 0 (aka 8), general knowledge
     var cat = "";
     if (category !== 8) {
         cat = '&category='+ category.toString();
     }
 
+    //array of difficulties; chooses one based on the level chosen by the user
     var arr= ["easy","medium","hard"];
     var dif = '&difficulty='+arr[difficulty];
 
+    //designates either multiple choice or boolean depending on the chosen value; default boolean
     var typ = '&type=';
     if(multi){
         typ += 'multiple';
     } else {
         typ += 'boolean';
     }
+    console.log('https://opentdb.com/api.php?'+num+cat+dif+typ);
 
-    console.log(num, cat, dif, typ);
 
     $.ajax({
         url: 'https://opentdb.com/api.php?'+num+cat+dif+typ,
@@ -48,11 +60,9 @@ function go(questions, category, difficulty, multi) {
                 console.log(result.results);
                 setUp(result.results);
             } else if (questions > 1) {
-                $('#warning').html("Not enough questions were found! You may have fewer questions or a different difficulty level.");
+                //if too many questions were answered, display error message and try with fewer questions
+                $('#warning').html("Not enough questions were found! You may recieve fewer questions than you asked. <br> Working...");
                 go(questions-1, category, difficulty, multi);
-
-            } else if (difficulty !== 0) {
-                go(FIRST_QS, category, difficulty-1, multi);
             }
 
         },
@@ -64,41 +74,53 @@ function go(questions, category, difficulty, multi) {
 
 function setUp(resultArray) {
     var category = parseInt(document.getElementById("category").selectedIndex);
+
     if(category === 0) {
         category = 9;
     } else {
         category += 8;
     }
-    $('#cat-dif').html($('#'+category).html()+": "+resultArray[0].difficulty)
+
+    //capitalizes first letter of the difficulty level to make it look nicer
+    console.log(resultArray);
+    var diff = resultArray[0].difficulty;//[0].toUpperCase() + resultArray[0].difficulty.slice(1,resultArray[0].difficulty.length);
+
+    $('#cat-dif').html(diff + " " +$('#'+category).html());
+
     var fin = "";
+
     for (var i = 0; i < resultArray.length; i++) {
+        //creates set of divs to hold info; each one after the first is invisible
         if(i > 0) {
             fin += "<div class = 'secret' id = 'contain" + i + "'>";
         } else {
             fin += "<div class = 'container' id = 'contain" + i + "'>";
         }
+        //the trivia question
         fin += "<span class = 'question' id = 'quest"+i+"'>"+resultArray[i].question+"</span>";
-        fin += "<span class = 'actually' id = 'act"+i+"'></span>";
-        fin += "<span class = 'buttons' id = 'choices"+i+"'>";
+        //area for the choice options
+        fin += "<br><span id = 'choices"+i+"'>";
 
-        var answers = [];
-
-        answers = answers.concat(resultArray[i].incorrect_answers);
-
+        //array to hole all answers
+        var answers = resultArray[i].incorrect_answers;
+        //gets a random position....
         var pos = Math.floor(Math.random() * answers.length);
-
+        //and inserts the right answer into the array at that position
         answers.splice(pos, 0, resultArray[i].correct_answer);
 
         for(var x = 0; x < answers.length; x ++) {
-
+            //creates a radio button for each answer with the correct class
             if(x === pos) {
+                //for the right answer
                 fin += "<input type = 'radio' class = 'rightanswer' id = 'right"+i+"' name = 'choice"+i+"' value = '"+answers[x]+"'>"
             } else {
-                fin += "<input type = 'radio'  class = 'wronganswer' id = '"+x+"wrong"+i+"' name = 'choice"+i+"' value = '"+answers[x]+"'>";
+                //for the wrong answer
+                fin += "<input type = 'radio' class = 'wronganswer' name = 'choice"+i+"' value = '"+answers[x]+"'>";
             }
-
-            fin += "<span id = 'answer"+x+"'>"+answers[x]+"</span><br>";
+            //labels the choice with the relevant option text
+            fin += "<label for = 'choice"+x+"'>"+answers[x]+"</label><br>";
         }
+        //when the button is clicked, moves to the check-answer function
         fin += "</span><button id = 'finished' onclick = 'moveOn("+i+", "+resultArray.length+");'>Finished!</button></div>";
     }
 
@@ -107,8 +129,8 @@ function setUp(resultArray) {
 
 function moveOn(i, length) {
 
-    var next = i+1;
     //1 more than the element we're on
+    var next = i+1;
 
     $('#contain'+i).hide();
 
@@ -116,29 +138,30 @@ function moveOn(i, length) {
         //show the next element
         document.getElementById('contain'+next).classList.remove('secret');
         document.getElementById('contain'+next).classList.add('container');
-
+    } else {
+        //display a final message w/ % correct and sundries
     }
     var choiceArr = document.getElementsByName('choice'+i); // list of the choice radio buttons
 
     for(var x = 0; x < choiceArr.length; x ++) {
 
         if(choiceArr[x].checked) {
-            //if the current choice was selected, check if it's correct
+            //if the current option was selected (effectively only checking the selected option), check if it's correct
             if(choiceArr[x].className === 'rightanswer') {
                 //if they got it right, display a win message
-
                 document.getElementById('result').className = 'correct';
-                CORRECT_ANSWERS++;
                 $('#result').html('Question ' + next + ' of '+ length+': '+$('#quest'+i).html() + '<br> Correct! You chose ' +choiceArr[x].value);
-
+                //increments global var SCORE (stays the same throughout a session)
+                SCORE ++;
             } else {
-                //if not, display a lose message
+                //if not, display a lose message with the correct answer
                 document.getElementById('result').className = 'incorrect';
                 var rightAns = document.getElementById('right'+i).value;
                 $('#result').html('Question ' + next + ' of '+ length+': '+$('#quest'+i).html() + '<br> Sorry! You picked '+choiceArr[x].value+' but you should have picked '+rightAns+'.');
             }
         }
     }
-    //update score
-    $('#score').html("Score: "+CORRECT_ANSWERS + "/"+length);
+    //updates relative score (out of total)
+    QS_SO_FAR ++;
+    $('#score').html("Score: "+SCORE + "/"+QS_SO_FAR);
 }
